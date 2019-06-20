88c96ebbbe8ff36da848e6334347edb26efb0462
Amy Tang <amytxng@gmail.com>
2019-06-20 11:56:27 -0400

    Create cell object

---
+++ cellObject.R
@@ -0,0 +1,14 @@
+# extract from phenodata file
+phenodata <- read.csv("C:/Users/amytx/OneDrive - UHN/tggates/phenowLot.csv",sep=",",header=TRUE, stringsAsFactors=TRUE)
+
+# subset necessary columns
+cellObject <- subset(phenodata, select=c(X, ORGAN_ID, MATERIAL_ID, SPECIES, TEST_TYPE, CELL_LOT_TYPE))
+
+# assign row names as barcode 
+rownames(cellObject)<-X
+
+# rename column names
+names(cellObject)<-c("cellid","tissueid","materialid", "species","testType","batchid")
+
+# save 
+save(cellObject, file="cellObject.csv")
